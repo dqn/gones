89bb8794a869396e3013fa1e13dd2bf900dab139
dqn <suyarizzz@gmail.com>
2020-04-10 02:45:30 +0900

    updat: opes

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -18,6 +18,10 @@ type CPU struct {
 	Memory    []byte
 }
 
+func nthBit(value byte, n uint8) uint8 {
+	return uint8((value >> n) & 1)
+}
+
 func (c *CPU) readByte(addr word) byte {
 	return c.Memory[addr]
 }
@@ -36,30 +40,111 @@ func (n *CPU) fetch() byte {
 	return n.readByte(addr)
 }
 
+func (c *CPU) getByteByMode(opeland word, mode string) byte {
+	switch mode {
+	case "immediate":
+		return byte(opeland)
+	default:
+		return c.readByte(opeland)
+	}
+}
+
 func (c *CPU) exec(baseName string, opeland word, mode string) {
 	switch baseName {
 	case "ADC":
-		var m byte
-		if mode == "immediate" {
-			m = byte(opeland)
-		} else {
-			m = c.readByte(opeland)
-		}
+		m := c.getByteByMode(opeland, mode)
 		if c.Registers.P.C {
 			m++
 		}
 		c.Registers.P.V = c.Registers.A < 0x80 && c.Registers.A+m > 0x7F
 		c.Registers.P.C = c.Registers.A+m <= c.Registers.A
 		c.Registers.A += m
-		c.Registers.P.N = c.Registers.A>>7 == 1
+		c.Registers.P.N = nthBit(c.Registers.A, 7) == 1
 		c.Registers.P.Z = c.Registers.A == 0
-	case "LDA":
-		if mode == "immediate" {
-			c.Registers.A = byte(opeland)
-		} else {
-			c.Registers.A = c.readByte(opeland)
+	case "SBC":
+		m := c.getByteByMode(opeland, mode)
+		if c.Registers.P.C {
+			m++
 		}
-		c.Registers.P.N = c.Registers.A>>7 == 1
+		c.Registers.P.V = c.Registers.A > 0x7F && c.Registers.A-m < 0x80
+		c.Registers.P.C = c.Registers.A-m >= c.Registers.A
+		c.Registers.A -= m
+		c.Registers.P.N = nthBit(c.Registers.A, 7) == 1
+		c.Registers.P.Z = c.Registers.A == 0
+	case "AND":
+		c.Registers.A &= c.getByteByMode(opeland, mode)
+		c.Registers.P.N = nthBit(c.Registers.A, 7) == 1
+		c.Registers.P.Z = c.Registers.A == 0
+	case "ORA":
+		c.Registers.A |= c.getByteByMode(opeland, mode)
+		c.Registers.P.N = nthBit(c.Registers.A, 7) == 1
+		c.Registers.P.Z = c.Registers.A == 0
+	case "EOR":
+		c.Registers.A ^= c.getByteByMode(opeland, mode)
+		c.Registers.P.N = nthBit(c.Registers.A, 7) == 1
+		c.Registers.P.Z = c.Registers.A == 0
+	case "ASL":
+		if mode == "accumulator" {
+			c.Registers.P.C = nthBit(c.Registers.A, 7) == 1
+			c.Registers.A <<= 1
+			c.Registers.P.N = nthBit(c.Registers.A, 7) == 1
+			c.Registers.P.Z = c.Registers.A == 0
+		} else {
+			data := c.readByte(opeland)
+			c.Registers.P.C = nthBit(data, 7) == 1
+			data <<= 1
+			c.write(opeland, data)
+			c.Registers.P.N = nthBit(data, 7) == 1
+			c.Registers.P.Z = data == 0
+		}
+	case "LSR":
+		if mode == "accumulator" {
+			c.Registers.P.C = nthBit(c.Registers.A, 0) == 1
+			c.Registers.A >>= 1
+			c.Registers.P.Z = c.Registers.A == 0
+		} else {
+			data := c.readByte(opeland)
+			c.Registers.P.C = nthBit(data, 0) == 1
+			data >>= 1
+			c.write(opeland, data)
+			c.Registers.P.Z = data == 0
+		}
+		c.Registers.P.N = false
+	case "ROL":
+		if mode == "accumulator" {
+			b := nthBit(c.Registers.A, 7)
+			c.Registers.P.C = b == 1
+			c.Registers.A = c.Registers.A<<1 + b
+			c.Registers.P.N = nthBit(c.Registers.A, 7) == 1
+			c.Registers.P.Z = c.Registers.A == 0
+		} else {
+			data := c.readByte(opeland)
+			b := nthBit(data, 7)
+			c.Registers.P.C = b == 1
+			data = data<<1 + b
+			c.write(opeland, data)
+			c.Registers.P.N = nthBit(data, 7) == 1
+			c.Registers.P.Z = data == 0
+		}
+	case "ROR":
+		if mode == "accumulator" {
+			b := nthBit(c.Registers.A, 0)
+			c.Registers.P.C = b == 1
+			c.Registers.A = c.Registers.A>>1 + b<<7
+			c.Registers.P.N = nthBit(c.Registers.A, 7) == 1
+			c.Registers.P.Z = c.Registers.A == 0
+		} else {
+			data := c.readByte(opeland)
+			b := nthBit(data, 0)
+			c.Registers.P.C = b == 1
+			data = data>>1 + b<<7
+			c.write(opeland, data)
+			c.Registers.P.N = nthBit(data, 7) == 1
+			c.Registers.P.Z = data == 0
+		}
+	case "LDA":
+		c.Registers.A = c.getByteByMode(opeland, mode)
+		c.Registers.P.N = nthBit(c.Registers.A, 7) == 1
 		c.Registers.P.Z = c.Registers.A == 0
 	case "STA":
 		c.write(opeland, c.Registers.A)
