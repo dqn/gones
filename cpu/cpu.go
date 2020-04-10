5f16c525bef4e857783b2c19eb791a6b8fddf0b2
dqn <suyarizzz@gmail.com>
2020-04-10 13:03:50 +0900

    update: add instructions

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,5 +1,7 @@
 package cpu
 
+import "fmt"
+
 type word = uint16
 
 type StatusRegister struct {
@@ -65,8 +67,8 @@ func (c *CPU) getByteByMode(opeland word, mode string) byte {
 	}
 }
 
-func (c *CPU) exec(baseName string, opeland word, mode string) {
-	switch baseName {
+func (c *CPU) exec(instruction string, opeland word, mode string) error {
+	switch instruction {
 	case "ADC":
 		m := c.getByteByMode(opeland, mode)
 		if c.Registers.P.C {
@@ -179,6 +181,10 @@ func (c *CPU) exec(baseName string, opeland word, mode string) {
 		if !c.Registers.P.V {
 			c.Registers.PC = opeland
 		}
+	case "BVS":
+		if c.Registers.P.V {
+			c.Registers.PC = opeland
+		}
 	case "BPL":
 		if !c.Registers.P.N {
 			c.Registers.PC = opeland
@@ -200,13 +206,128 @@ func (c *CPU) exec(baseName string, opeland word, mode string) {
 		c.push(byte(pc))
 	case "RTS":
 		c.Registers.PC = word(c.pop()) + word(c.pop())<<8 + 1
+	case "BRK": // TODO
+		// if !c.Registers.P.I {
+		// 	c.Registers.P.B = true
+		// 	c.Registers.PC++
+		// 	c.push(byte(c.Registers.PC >> 8))
+		// 	c.push(byte(c.Registers.PC))
+		// 	c.push(byte(c.Registers.P))
+		// }
+	case "RTI": // TODO
+		// c.Registers.P = c.pop()
+		// c.Registers.PC = word(c.pop()) + word(c.pop())<<8 + 1
+	case "CMP":
+		data := c.Registers.A - c.getByteByMode(opeland, mode)
+		c.Registers.P.N = isNegative(data)
+		c.Registers.P.Z = data == 0
+		c.Registers.P.C = data >= c.Registers.A
+	case "CPX":
+		data := c.Registers.X - c.getByteByMode(opeland, mode)
+		c.Registers.P.N = isNegative(data)
+		c.Registers.P.Z = data == 0
+		c.Registers.P.C = data >= c.Registers.X
+	case "CPY":
+		data := c.Registers.Y - c.getByteByMode(opeland, mode)
+		c.Registers.P.N = isNegative(data)
+		c.Registers.P.Z = data == 0
+		c.Registers.P.C = data >= c.Registers.Y
+	case "INC":
+		m := c.readByte(opeland) + 1
+		c.Registers.P.N = isNegative(m)
+		c.Registers.P.Z = m == 0
+		c.write(opeland, m)
+	case "DEC":
+		m := c.readByte(opeland) - 1
+		c.Registers.P.N = isNegative(m)
+		c.Registers.P.Z = m == 0
+		c.write(opeland, m)
+	case "INX":
+		c.Registers.X++
+		c.Registers.P.N = isNegative(c.Registers.X)
+		c.Registers.P.Z = c.Registers.X == 0
+	case "DEX":
+		c.Registers.X--
+		c.Registers.P.N = isNegative(c.Registers.X)
+		c.Registers.P.Z = c.Registers.X == 0
+	case "INY":
+		c.Registers.Y++
+		c.Registers.P.N = isNegative(c.Registers.Y)
+		c.Registers.P.Z = c.Registers.Y == 0
+	case "DEY":
+		c.Registers.Y--
+		c.Registers.P.N = isNegative(c.Registers.Y)
+		c.Registers.P.Z = c.Registers.Y == 0
+	case "CLC":
+		c.Registers.P.C = false
+	case "SEC":
+		c.Registers.P.C = true
+	case "CLI":
+		c.Registers.P.I = false
+	case "SEI":
+		c.Registers.P.I = true
+	case "CLD":
+		c.Registers.P.D = false
+	case "SED":
+		c.Registers.P.D = true
+	case "CLV":
+		c.Registers.P.V = false
 	case "LDA":
 		c.Registers.A = c.getByteByMode(opeland, mode)
 		c.Registers.P.N = isNegative(c.Registers.A)
 		c.Registers.P.Z = c.Registers.A == 0
+	case "LDX":
+		c.Registers.X = c.getByteByMode(opeland, mode)
+		c.Registers.P.N = isNegative(c.Registers.X)
+		c.Registers.P.Z = c.Registers.X == 0
+	case "LDY":
+		c.Registers.Y = c.getByteByMode(opeland, mode)
+		c.Registers.P.N = isNegative(c.Registers.Y)
+		c.Registers.P.Z = c.Registers.Y == 0
 	case "STA":
 		c.write(opeland, c.Registers.A)
+	case "STX":
+		c.write(opeland, c.Registers.X)
+	case "STY":
+		c.write(opeland, c.Registers.Y)
+	case "TXA":
+		c.Registers.A = c.Registers.X
+		c.Registers.P.N = isNegative(c.Registers.A)
+		c.Registers.P.Z = c.Registers.A == 0
+	case "TAX":
+		c.Registers.X = c.Registers.A
+		c.Registers.P.N = isNegative(c.Registers.X)
+		c.Registers.P.Z = c.Registers.X == 0
+	case "TAY":
+		c.Registers.Y = c.Registers.A
+		c.Registers.P.N = isNegative(c.Registers.Y)
+		c.Registers.P.Z = c.Registers.Y == 0
+	case "TYA":
+		c.Registers.A = c.Registers.Y
+		c.Registers.P.N = isNegative(c.Registers.A)
+		c.Registers.P.Z = c.Registers.A == 0
+	case "TSX":
+		c.Registers.X = byte(c.Registers.SP)
+		c.Registers.P.N = isNegative(c.Registers.X)
+		c.Registers.P.Z = c.Registers.X == 0
+	case "TXS":
+		c.Registers.SP = word(c.Registers.X)
+	case "PHA":
+		c.push(c.Registers.A)
+	case "PLA":
+		c.Registers.A = c.pop()
+		c.Registers.P.N = isNegative(c.Registers.A)
+		c.Registers.P.Z = c.Registers.A == 0
+	case "PHP":
+		// TODO
+	case "PLP":
+		// TODO
+	case "NOP":
+		// no operation
+	default:
+		return fmt.Errorf("unknown instruction: %s", instruction)
 	}
+	return nil
 }
 
 func (c *CPU) Reset() {
